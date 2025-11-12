deda05e056d834419794847717cb3080ee125821
DINESH S <119110395+Dinesh-122333@users.noreply.github.com>
2025-11-12 22:35:43 +0530

    Add LeetCode and Companies components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,20 @@ import Home from './component/Home'
 import Works from './component/Works'
 import Aboutus from './component/About'
 import Resume from './component/Resume'
+import LeetCode from './component/LeetCode'
+import Companies from './component/Companies'
 import Contact from './component/Contact'
-
 export default function App() {
-  return(
-    <>
-    <Nav/>
-    <Home/>
-    <Works/>
-    <Aboutus/>
-    <Resume/>
-    <Contact/>
-    </>
-  )
+ return(
+ <>
+ <Nav/>
+ <Home/>
+ <Works/>
+ <LeetCode/>
+ <Companies/>
+ <Aboutus/>
+ <Resume/>
+ <Contact/>
+ </>
+ )
 }
-
-
